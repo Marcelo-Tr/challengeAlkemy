1ce2d4fb0857c509b83aa17c1c5759a5612e4004
Marcelo <marcelo.trini@gmail.com>
2022-05-24 12:46:23 -0300

    se quita mongo y se agrega MYSQL

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const dbConnect = require('./config/mongo') //IMPORT DB inportando modulo de dbConnect creado en config
 const app = express()
+const {db, dbConnectMySql} = require("./config/mysql")
 
 app.use(cors())
 
-const port = process.env.PORT || 3000 //PORT estableciendo puerto de conexion y el default en 3000
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
-
-dbConnect()
+dbConnectMySql()
